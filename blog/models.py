6c740c0021a4309fad66c9ab26930dcba45ab956
Roman Podolets <romanpyu@gmail.com>
2023-09-30 13:55:07 +0300

    Add str for models and updated requirements

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,9 @@ class User(AbstractUser):
 class Tag(models.Model):
     name = models.CharField(max_length=31, unique=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -19,9 +22,15 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, related_name="posts")
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="comments", on_delete=models.CASCADE)
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.user.username} at {self.created_at}"
