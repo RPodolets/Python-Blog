f6eb9aa3ef4c5958fee41981587b0a68486d3e18
Roman Podolets <romanpyu@gmail.com>
2023-09-30 14:03:29 +0300

    Add pagination for index page

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ class PostListView(generic.ListView):
     model = Post
     queryset = Post.objects.select_related("user").prefetch_related("tags").order_by("-created_at")
     template_name = "blog/index.html"
+    paginate_by = 5
 
 
 class PostDetailView(generic.DetailView):
