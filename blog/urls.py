6bc9b83451211914dd64cf3fdd65d1f820537e80
Roman Podolets <romanpyu@gmail.com>
2023-10-01 23:03:11 +0300

    Add comment system

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from blog.views import PostListView, PostDetailView
+from blog.views import PostListView, post_detail
 
 urlpatterns = [
     path("", PostListView.as_view(), name="index"),
-    path("<int:pk>/", PostDetailView.as_view(), name="post_detail")
+    path("<int:pk>/", post_detail, name="post_detail")
 ]
 
 app_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
-from blog.models import Post, Tag
+from blog.forms import CommentForm
+from blog.models import Post, Tag, Comment
 
 
 class PostListView(generic.ListView):
@@ -18,7 +20,34 @@ class PostListView(generic.ListView):
             ).select_related("user").prefetch_related().order_by("-created_at")
         return Post.objects.select_related("user").prefetch_related("tags").order_by("-created_at")
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag_list"] = Tag.objects.all()
+        print(context.keys())
+        return context
 
-class PostDetailView(generic.DetailView):
-    model = Post
-    queryset = Post.objects.select_related("user").prefetch_related("tags").order_by("-created_at")
+
+def post_detail(request, pk):
+    template_name = 'blog/post_detail.html'
+    post = get_object_or_404(Post, pk=pk)
+    comments = Comment.objects.filter(post__id=pk)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            user = get_user_model().objects.get(id=request.user.id)
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.user = user
+            new_comment.save()
+            return HttpResponseRedirect(f"/{pk}")
+
+    comment_form = CommentForm()
+    context = {
+        "post": post,
+        "comments": comments,
+        "new_comment": new_comment,
+        "comment_form": comment_form
+    }
+    return render(request, template_name, context=context)
